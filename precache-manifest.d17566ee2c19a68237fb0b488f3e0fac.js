89663fa6ead4a788fed8bbe7f258c90216838736
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2019-04-06 15:05:07 -0400

    Updates

---
+++ precache-manifest.d17566ee2c19a68237fb0b488f3e0fac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mirador3-with-collection-sidebar/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dc8b73e123fb78c0478c",
+    "url": "/mirador3-with-collection-sidebar/static/js/runtime~main.2d3a06c6.js"
+  },
+  {
+    "revision": "e699bfe58e040c1c2284",
+    "url": "/mirador3-with-collection-sidebar/static/js/main.50b04d15.chunk.js"
+  },
+  {
+    "revision": "f8e90d96cb5b4b293d4e",
+    "url": "/mirador3-with-collection-sidebar/static/js/2.ffe46921.chunk.js"
+  },
+  {
+    "revision": "e699bfe58e040c1c2284",
+    "url": "/mirador3-with-collection-sidebar/static/css/main.daed6982.chunk.css"
+  },
+  {
+    "revision": "f8e90d96cb5b4b293d4e",
+    "url": "/mirador3-with-collection-sidebar/static/css/2.1123ce88.chunk.css"
+  },
+  {
+    "revision": "cc73e1d0f7a1676dc0d27e166e8c6f41",
+    "url": "/mirador3-with-collection-sidebar/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/mirador3-with-collection-sidebar/precache-manifest.d17566ee2c19a68237fb0b488f3e0fac.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/mirador3-with-collection-sidebar/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
